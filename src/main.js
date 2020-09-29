dfbe17c20ad9a74c6387b97089eb92c71ccb68ef
ridoNev1 <ridolaurent021123@gmail.com>
2020-09-29 15:38:04 +0700

    update main.js

--- src/main.js
+++ src/main.js
@@ -5,20 +5,16 @@ import store from './store'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-<<<<<<< HEAD
 import VueSweetalert2 from 'vue-sweetalert2'
-=======
 import axios from 'axios'
 import { url } from './helper/env'
->>>>>>> 960926f70ccbab4b6ec8b312fe073c40c3ad1d95
 
 Vue.config.productionTip = false
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
-<<<<<<< HEAD
 Vue.use(VueSweetalert2)
-=======
+
 axios.defaults.headers = {
   token: localStorage.getItem('token')
 }
@@ -40,7 +36,6 @@ axios.interceptors.response.use((response) => {
     return response
   }
 })
->>>>>>> 960926f70ccbab4b6ec8b312fe073c40c3ad1d95
 
 new Vue({
   router,
